fbb1edb91390be7c1ddadb92affb3704e2974010
kiclu <kiclu@protonmail.com>
2023-03-14 01:33:25 +0100

    fixed addresses in mif

--- mifgen.py
+++ mifgen.py
@@ -7,26 +7,29 @@ def insch(my_str, group=2, char=' '):
     my_str = str(my_str)
     return char.join(my_str[i:i+group] for i in range(0, len(my_str), group))
 
+def pack(addr):
+    return hex(int(addr, 16)//4)[2:]
+
 def split_words(dump):
     cnt = 0
     for line in dump:
         line[1] = insch(line[1])
-        tmp = line[1].split(' ')[::-1]
-        cnt += len(tmp)
-        line[1] = ' '.join(tmp)
-        print(line[0] + ' : ' + line[1])
+        tmp = line[1].split(' ')
+        cnt += 1
+        line[1] = ''.join(tmp)
+        print(pack(line[0]) + ' : ' + line[1])
     return cnt
 
 def write_mifdump(dump, filename, cnt):
     with open(filename, 'w') as out:
         out.write('DEPTH = ' + str(cnt) + ';\n')
-        out.write('WIDTH = 8;\n')
+        out.write('WIDTH = 32;\n')
         out.write('ADDRESS_RADIX = HEX;\n')
         out.write('DATA_RADIX = HEX;\n')
         out.write('CONTENT\n')
         out.write('BEGIN\n')
         for (addr, data) in dump:
-            out.write(addr + ' : ' + data + ';\n')
+            out.write(pack(addr) + ' : ' + data + ';\n')
         out.write('END;\n')
 
 if len(sys.argv) != 3:
